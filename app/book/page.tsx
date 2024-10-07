6896f2b05141ceb62d79919419960b0a877e3b1c
kilian-bellot <kilian.bellot.partner@decathlon.com>
2024-10-07 08:07:21 -0400

    feat: add book page

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,32 +1,35 @@
 import Page from "@app/_components/Page";
+import Section from "@app/_components/Section";
+import MediaTextColumnLayout from "@app/_components/MediaTextColumnLayout";
+import elephantImage from "@/public/assets/book/elephant.png";
+import athenImage from "@/public/assets/book/athen.png";
+import Divider from "@app/_components/Divider";
 
 export default function Book() {
   return (
     <Page>
-      {/*<Section title={"Votre carnet de voyage"} className={"flex flex-col md:flex-row"}>*/}
-      {/*  <MediaTextColumnLayout image={easterIsleImage} alt={"statues Moaï sur l'île de paques"}>*/}
-      {/*    <p>*/}
-      {/*      Pour chaque voyage conçu avec moi, vous recevez un carnet de voyage 100% personnalisé, réalisé uniquement*/}
-      {/*      pour votre voyage.*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      Je m'assure que mes carnets soient à la fois riches en informations et agréables à lire. Vous recevrez une*/}
-      {/*      version numérique, facile à sauvegarder et à consulter. Ce guide vous facilitera l'organisation de votre*/}
-      {/*      voyage et vous aidera à en maximiser les expériences.*/}
-      {/*    </p>*/}
-      {/*  </MediaTextColumnLayout>*/}
-      {/*  <Divider/>*/}
-      {/*  <MediaTextColumnLayout image={maldivesImage} alt={"une ile des maldives"} reverse>*/}
-      {/*    <p className={"font-bold"}>*/}
-      {/*      Quand allez-vous recevoir votre carnet de voyage personnalisé ?*/}
-      {/*    </p>*/}
-      {/*    <p>*/}
-      {/*      Une fois votre voyage validé, je réalise votre carnet de voyage en y incluant les réservations que vous avez*/}
-      {/*      effectuées directement suite à mes propositions. Je m’engage à vous le transmettre au moins 15 jours avant*/}
-      {/*      votre départ en voyage.*/}
-      {/*    </p>*/}
-      {/*  </MediaTextColumnLayout>*/}
-      {/*</Section>*/}
+      <Section title={"Votre carnet de voyage"} className={"flex flex-col md:flex-row"}>
+        <MediaTextColumnLayout image={elephantImage} alt={"une famille d'éléphants"}>
+          <p>
+            Pour chaque itinéraire que nous concevons ensemble, vous recevrez un carnet de voyage 100% personnalisé
+          </p>
+          <p>
+            Je m'assure que mes carnets soient à la fois riches en informations et agréables à lire. Vous recevrez une version numérique, facile à sauvegarder et à consulter.
+          </p>
+          <p>
+            Ce guide vous facilitera l'organisation de votre voyage et vous aidera à en maximiser les expériences.
+          </p>
+        </MediaTextColumnLayout>
+        <Divider/>
+        <MediaTextColumnLayout image={athenImage} alt={"une ile des maldives"} reverse>
+          <p className={"font-bold"}>
+            Quand allez-vous recevoir votre carnet de voyage ?
+          </p>
+          <p>
+            Une fois votre voyage validé, je réalise votre carnet de voyage en y incluant les réservations que vous avez effectuées directement suite à mes propositions. Je m’engage à vous le transmettre au moins 15 jours avant votre départ en voyage.
+          </p>
+        </MediaTextColumnLayout>
+      </Section>
       {/*<Section>*/}
       {/*  <p className={"font-bold"}>*/}
       {/*    Que contient votre carnet de voyage personnalisé ?*/}
@@ -48,7 +51,6 @@ export default function Book() {
       {/*    <li>Une carte interactive totalement personnalisée.</li>*/}
       {/*  </ul>*/}
       {/*</Section>*/}
-      WIP...
     </Page>
   );
 }
